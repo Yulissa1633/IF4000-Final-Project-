3b399699f072dfb9b63a1bff46cf02beb75af2f2
Yulissa1633 <yulissa@1633gmail.com>
2020-11-14 18:56:46 -0600

    Random generator 6

--- Project.s
+++ Project.s
@@ -18,15 +18,13 @@ main:	push	{r4, r5}	@ salva registros
 	mov	r4, #6	@ inicializa contador del bucle
 
 bucle:	cmp	r4, #0	@ si llega a cero, sale
-	bne	end
-	
-cmptrue:bl	myrand	@ lee número aleatorio
+	bhi	end
+	bl	myrand	@ lee número aleatorio
 	mov	r1, r0	@ pasa valor a r1
 	ldr	r0, =var1	@ pone cadena de texto en r1
 	bl	printf	@ llama a la función printf
 	subs	r4, r4, #1	@ decremento contador	
-	b	cmptrue
-	pop	{r4, r5}	@ recupera registros y sale
+	b	bucle
 
 myrand:	ldr	r1, =seed	@ leo puntero semilla
 	ldr	r0, [r1]	@ leo valor de semilla
@@ -41,6 +39,7 @@ myrand:	ldr	r1, =seed	@ leo puntero semilla
 mysrand: ldr	r1, = seed	/*"seed >> 16 y 0x7 fff (evita AND)"*/
 	 str	r0, [r1]
 	 bx	lr
-end:	bx	lr
+end:	pop	{r4, r5}	@ recupera registros y sale
+	bx	lr
 
 
