8390bb050c710cb724a10ae7b22595c404734ca3
Yulissa1633 <yulissa1633@gmail.com>
2020-11-24 13:42:38 -0600

    Violación del segmento error

--- Project.s
+++ Project.s
@@ -4,7 +4,7 @@
 .data
 
 .balign 4
-randarray:	.skip	40000
+randarray:	.skip	4000000
 printf_str:     .asciz	"randarray[%d] = %d\n"
 debug_str:	.asciz	"R%-2d   0x%08X  %011d \n"
 exit_str:       .ascii	"Programa terminado.\n"
@@ -21,9 +21,9 @@ exit_str:       .ascii	"Programa terminado.\n"
 			<andrew@vaelen.org>*/
    
 main:		bl _seedrand            @ Toma el método seedrand
-		mov R0, #0              @ Inicializa índice de la variable
+		mov R0, #1              @ Inicializa índice de la variable
 
-writeloop:	cmp R0, #2000           @ Revisa que se haga la iteración
+writeloop:	cmp R0, #5           @ Revisa que se haga la iteración
 		beq writedone           @ Termina el ciclo si está listo
 	    	ldr R1, =randarray      @ Busca el arreglo
 	    	lsl R2, R0, #2          @ Multiplica el índice * 4 para tener el desplazamiento del arreglo
@@ -37,10 +37,10 @@ writeloop:	cmp R0, #2000           @ Revisa que se haga la iteración
 	    	add R0, R0, #1          @ Incrementa ínidce
 	    	b   writeloop           @ Vuelve al ciclo
 
-writedone:	mov R0, #0              @ Inicializa índice de la variable
+writedone:	mov R0, #1              @ Inicializa índice de la variable
 
-readloop:	cmp R0, #2000           @ Revisa que se haga la iteración
-		beq readdone            @ Termina el ciclo si está listo
+readloop:	cmp R0, #5           @ Revisa que se haga la iteración
+		beq bsort_next            @ Termina el ciclo si está listo
 		ldr R1, =randarray      @ Busca el arreglo
 		lsl R2, R0, #2          @ Multiplica el índice * 4 para tener el desplazamiento del arreglo
 		add R2, R1, R2          @ Pasa el elemento a R2
@@ -53,18 +53,17 @@ readloop:	cmp R0, #2000           @ Revisa que se haga la iteración
 		bl  _printf             @ Llama al procedimiento que imprime
 		pop {R2}                @ Restaura registro
 		pop {R1}                @ Restaura registro
-		pop {R0}	@ Restaura registro
+		pop {R0}		@ Restaura registro
 		add R0, R0, #1          @ Incrementa el índice
 		b   readloop            @ Vuelve al ciclo
 
 		    /*R0 = Tamaño del arreglo, 
 		    R1 = Ubicación del arreglo*/
 
-		push {R0-R6,LR}	@ Salvar registros
+bsort_next:	push {R0-R6, LR}
 		ldr R1, =randarray
-		mov R0, #2000
-
-bsort_next:	mov R2,#0	@ R2 = Número del elemento actual
+		mov R0, #5
+		mov R2,#0	@ R2 = Número del elemento actual
                 mov R6,#0       @ R6 = Contador 
 
 bsort_loop:	add R3,R2,#1	@ R3 = Siguente número
@@ -77,17 +76,15 @@ bsort_loop:	add R3,R2,#1	@ R3 = Siguente número
     		strgt R4,[R0,R3,LSL #2]	@ Si R4 > R5, almacena el siguiente valor en el actual
     		addgt R6,R6,#1	@ Si R4 > R5, Incrementa contador
     		mov R2,R3	@ Avanza al siguiente elemento 
-    		b bsort_loop 	@ Fin del ciclo
+    		b bsort_loop 	@ Vuelve al ciclo
 
-printagain:	b readloop	@ Imprime de nuevo cuando esté ordenado
-
-readdone:	pop {R0-R6,PC}	@ Saca los registros y sale
-	    	b _exit                 @ Sale si está listo
+readdone:    	pop {R0-R6,PC}
+		b _exit                 @ Sale si está listo
     
 _exit:  	mov R7, #4              
 	    	mov R0, #1              
-	    	mov R2, #21             @ Print string length
-	    	ldr R1, =exit_str       @ String at label exit_str:
+	    	mov R2, #21             @ Imprimir el largo del string
+	    	ldr R1, =exit_str       @ Mensaje de salida exit_str:
 	    	swi 0                   @ Hace llamada al sistema
 	    	mov R7, #1              @ Termina llamada al sistema, 1
 	    	swi 0                   @ Hace llamada al sistema
@@ -99,18 +96,16 @@ _printf:	push {LR}               @ Almacena retorno
     
 _seedrand:    	push {LR}               @ Respaldo al retorno
 	    	mov R0, #0              @ Pasa 0 para llamar a time
-	    	bl time                 @ Toma time
+	    	bl time                 @ Toma time del sistema
 	    	mov R1, R0              @ Pasa time a srand
 	    	bl srand                @ Toma srand
 	    	pop {PC}                @ Retorna 
     
 _getrand:    	push {LR}               @ Respaldo al retorno
-	    	bl rand                 @ Roma el número aleatorio
+	    	bl rand                 @ Toma el número aleatorio
 	    	pop {PC}                @ Retorna 
-
-		
-
 
 bsort_check:	cmp R6,#0	@ Revisa cambios, ¿han habido cambios en esta iteración?
     		subgt R1,R1,#1	@ En el siguiente ciclo omite el último valor
-    		bgt bsort_next	@ Se hace de nuevo si hubieron cambios
+    		bgt bsort_next	@ Se hace de nuevo si hubieron cambios		
+		b readdone
